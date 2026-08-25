9ab0566f36d39ba433c17206a0a2a75c6053bda0
Isaac Adebisi <138534696+kenolsph@users.noreply.github.com>
2026-08-25 15:03:25 +0000

    ololo

--- main.py
+++ main.py
@@ -39,13 +39,17 @@ def find_invalid_records(
     patient_id, age, gender, diagnosis, medications, last_visit_id
 ):
     constraints = {
-        'patient_id': isinstance(patient_id, str) and re.fullmatch('p\d+', patient_id, re.IGNORECASE),
+        'patient_id': isinstance(patient_id, str)
+        and re.fullmatch('p\d+', patient_id, re.IGNORECASE),
         'age': isinstance(age, int) and age >= 18,
         'gender': isinstance(gender, str) and gender.lower() in ('male', 'female'),
         'diagnosis': isinstance(diagnosis, str) or diagnosis is None,
-        'medications': isinstance(medications, list) and [isinstance(i, str) for i in medications]
+        'medications': isinstance(medications, list)
+        and all([isinstance(i, str) for i in medications]),
+        'last_visit_id': isinstance(last_visit_id, str)
+        and re.fullmatch('v\d+', last_visit_id, re.IGNORECASE)
     }
-    return constraints
+    return [key for key, value in constraints.items() if not value]
 
 def validate(data):
     is_sequence = isinstance(data, (list, tuple))
@@ -63,6 +67,7 @@ def validate(data):
         if not isinstance(dictionary, dict):
             print(f'Invalid format: expected a dictionary at position {index}.')
             is_invalid = True
+            
 
         if set(dictionary.keys()) != key_set:
             print(
@@ -70,10 +75,11 @@ def validate(data):
             )
             is_invalid = True
 
+        invalid_records = find_invalid_records(**dictionary)
+
     if is_invalid:
         return False
     print('Valid format.')
     return True
 
 validate(medical_records)
-print(find_invalid_records(**medical_records[0]))
