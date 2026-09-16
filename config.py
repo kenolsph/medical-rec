3e32e1cc5ef88a41d4995c30f188dbf4b6b86769
Isaac Adebisi <138534696+kenolsph@users.noreply.github.com>
2026-09-16 10:35:34 +0100

    Update config.py

--- config.py
+++ config.py
@@ -84,9 +84,15 @@ class Inbox:
 
 def main():
     tory = User('Tory')
-    ramy = User('Ramy')
+    ramy = User('Ramy')        
     
     tory.send_email(ramy, 'Hello', 'Hi Ramy, just saying hello!')
     ramy.send_email(tory, 'Re: Hello', 'Hi Tory, hope you are fine.')
+
+    ramy.check_inbox()
+    ramy.read_email(1)
+    ramy.delete_email(1)
+    ramy.check_inbox()
+
 if __name__ == '__main__':
     main()
