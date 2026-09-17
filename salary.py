a31a979d84793d3ccb46435b764c040c977438e8
Isaac Adebisi <138534696+kenolsph@users.noreply.github.com>
2026-09-17 14:52:50 +0100

    Refactor Employee class with properties
    Refactored Employee class to use properties for name and level.

--- salary.py
+++ salary.py
@@ -1,12 +1,22 @@
 class Employee:
     def __init__(self, name, level):
+        
         self._name = name
         self._level = level
-
+        
     def __str__(self):
-        return f'{self._name}: {self._level}'
+        return f'{self.name}: {self.level}'
 
-    
+    def __repr__(self):
+        return f"Employee('{self.name}', '{self.level}')"
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def level(self):
+        return self._level
 
 charlie_brown = Employee('Charlie Brown', 'trainee')
 print(charlie_brown)
