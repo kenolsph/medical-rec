fdf633a5614820df9b9507ccf6b18e9e0a76bda4
Isaac Adebisi <138534696+kenolsph@users.noreply.github.com>
2026-09-17 15:12:36 +0100

    Define base salaries for employee levels
    Added a class variable for base salaries based on employee levels.

--- salary.py
+++ salary.py
@@ -1,9 +1,20 @@
 class Employee:
+    _base_salaries = {
+        'trainee': 1000,
+        'junior': 2000,
+        'mid-level': 3000,
+        'senior': 4000,
+    }
+    
     def __init__(self, name, level):
-        
+        if not (isinstance(name, str) and isinstance(level, str)):
+            raise TypeError("'name' and 'level' attribute must be of type 'str'.")
+        if level not in Employee._base_salaries:
+            raise ValueError(f"Invalid value '{level}' for 'level' attribute.")
         self._name = name
         self._level = level
-        
+        self._salary = Employee._base_salaries[level]
+
     def __str__(self):
         return f'{self.name}: {self.level}'
 
