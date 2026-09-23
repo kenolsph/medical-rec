52fb41d81e4b8dfe32dd4cfee34cdaff0fba095c
Isaac Adebisi <138534696+kenolsph@users.noreply.github.com>
2026-09-23 08:36:39 +0100

    Implement discount strategy classes for products

--- disc_calc.py
+++ disc_calc.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 class Product:
     def __init__(self, name: str, price: float) -> None:
         self.name = name
@@ -6,5 +8,29 @@ class Product:
     def __str__(self) -> str:
         return f'{self.name} - ${self.price}'
 
+class DiscountStrategy(ABC):
+    @abstractmethod
+    def is_applicable(self, product: Product, user_tier: str) -> bool:
+        pass
+
+    @abstractmethod
+    def apply_discount(self, product: Product) -> float:
+        pass
+
+class PercentageDiscount(DiscountStrategy):
+    def __init__(self, percent: int) -> None:
+        self.percent = percent
+
+    def is_applicable(self, product: Product, user_tier: str) -> bool:
+        return self.percent <= 70
+
+    def apply_discount(self, product: Product) -> float:
+        return product.price * (1 - self.percent / 100)
+
+
+
 product = Product('Wireless Mouse', 50.0)
-print (product)
+print(product)
+
+discount = PercentageDiscount(10)
+print(discount.apply_discount(product))
